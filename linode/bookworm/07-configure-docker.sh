0b41d66aa1d972f8660a92fac636244bed80ad64
Karl Lew <karl@firepick.org>
2023-10-13 10:03:36 -0700

    v2.4.0 += bookworm

---
+++ linode/bookworm/07-configure-docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+DIR=`dirname $0`; 
+APPDIR=`realpath $DIR/../..`
+LOCALDIR=`realpath $APPDIR/local`
+SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+echo -e "${SCRIPT}: BEGIN `date`"
+
+set -e
+
+if [ "$SERVERNAME" == "" ]; then
+  read -p "$SCRIPT => Enter server_name (localhost): " SERVERNAME
+  if [ "$SERVERNAME" == "" ]; then
+      export SERVERNAME=localhost
+  fi
+fi
+
+echo -e "$SCRIPT: SERVERNAME is $SERVERNAME"
+DOCKER_VOLUME=nodejs_scv-local
+echo -e "$SCRIPT: linking to $DOCKER_VOLUME as ./local" 
+ln -s -f /var/lib/docker/volumes/$DOCKER_VOLUME/_data local
+
+echo -e "$SCRIPT: sudo docker compose up -d"
+sudo docker compose up -d
+
+echo -e "$SCRIPT: sudo cat local/scv.log"
+sudo cat local/scv.log
+
+URLPATH=scv/play/segment/thig1.1/en/sujato/thig1.1%3A1.1/Amy
+echo -e "$SCRIPT: testing localhost:8080..."
+curl http://localhost:8080/$URLPATH;
+echo
+echo -e "$SCRIPT: api.sc-voice.net docker container is running (OK)"
+
+echo -e "$SCRIPT: testing https://$SERVERNAME..."
+curl https://$SERVERNAME/$URLPATH
+echo
+echo -e "$SCRIPT: reverse proxy => api.sc-voice.net docker container (OK)"
+
+echo -e "${SCRIPT}: END `date`"
---
+++ linode/bookworm/08-cron.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+DIR=`dirname $0`; 
+APPDIR=`realpath $DIR/../..`
+LOCALDIR=`realpath $APPDIR/local`
+SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+echo -e "${SCRIPT}: BEGIN `date`"
+
+set -e
+
+crontab -l > /tmp/mycron
+if grep scv-cron /tmp/mycron; then
+  echo -e "$SCRIPT: scv-cron.sh job already configured"
+  echo -e "${SCRIPT}: END `date`"
+  exit
+fi
+
+echo -e "$SCRIPT: adding scv-cron.sh to cron "
+cat <<CRON_HEREDOC >> /tmp/mycron
+0 4,12,20 * * * /home/unroot/api.sc-voice.net/linode/bullseye/scv-cron.sh >> /home/unroot/scv-cron.log 2>&1
+CRON_HEREDOC
+
+crontab /tmp/mycron
+
+echo -e "${SCRIPT}: END `date`"
