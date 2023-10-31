086bd393dec6a087063b60e9791d58b523af37b0
Karl Lew <karl@firepick.org>
2023-10-31 13:05:04 -0700

    v2.30.0 += servername

--- linode/bookworm/05-certbot.sh
+++ linode/bookworm/05-certbot.sh
@@ -35,9 +35,10 @@ else
 fi
 
 if [ "$SERVERNAME" == "" ]; then
-  read -p "$SCRIPT => Enter server_name (localhost): " SERVERNAME
+  HOSTNAME=`hostname`
+  read -p "$SCRIPT => Enter server_name ($HOSTNAME): " SERVERNAME
   if [ "$SERVERNAME" == "" ]; then
-      export SERVERNAME=localhost
+      export SERVERNAME=$HOSTNAME
   fi
 fi
 echo -e "$SCRIPT: SERVERNAME is $SERVERNAME"
