c86414dba4ae59dfefa40f0ec994a6d26429419a
Karl Lew <karl@firepick.org>
2023-11-01 08:28:39 -0700

    v2.32.0 += version

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,4 +7,4 @@ set -e
 
 npm run build:dist
 VERSION=`$APPDIR/linode/bookworm/version.sh`
-sed -i -e "s/APISCV.*/APISCV=$VERSION/" $APPDIR/Dockerfile
+sed -i -e "s/APISCV_VERSION.*/APISCV_VERSION=$VERSION/" $APPDIR/Dockerfile
