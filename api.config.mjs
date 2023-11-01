c43ecd224377cc53c8a9f44798ad1e813adff2a2
Karl Lew <karl@firepick.org>
2023-10-31 18:49:18 -0700

    v2.31.0 += staging

--- api.config.mjs
+++ api.config.mjs
@@ -10,6 +10,14 @@ const ApiConfig = {
         ffree_percent: true
       },
     },{
+      url: 'https://staging.api.sc-voice.net/scv/statfs', 
+      type: "statfs",
+      jsonFilter: {
+        blocks: true,
+        bavail_percent: true,
+        ffree_percent: true
+      },
+    },{
       url: 'https://s1.sc-voice.net/scv/play/segment/thig1.1/en/soma/thig1.1%3A1.1/Amy',
       type: "play/segment",
       jsonFilter: {
