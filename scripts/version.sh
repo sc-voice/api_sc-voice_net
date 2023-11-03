936d1539481ba1a596d8c5a1791024bceef84bd0
Karl Lew <karl@firepick.org>
2023-11-02 21:27:37 -0700

    v2.37.0 += version

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,7 +5,7 @@ SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
 APP=$DIR/..
 set -e
 
-VERSION=`$APPDIR/linode/bookworm/version.sh`
+VERSION=`$DIR/git-version.sh`
 echo Dockerfile $VERSION
 sed -e "s/APISCV_VERSION.*/APISCV_VERSION=$VERSION/" -i $APPDIR/Dockerfile
 git add Dockerfile 
