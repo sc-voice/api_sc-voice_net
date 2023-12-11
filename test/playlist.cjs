fbede8f3c3dfedff56c5ab3b206cf026c96a5ae8
Karl Lew <karl@firepick.org>
2023-12-11 06:21:39 -0800

    v7.0.56 += test

--- test/playlist.cjs
+++ test/playlist.cjs
@@ -188,7 +188,7 @@ typeof describe === "function" &&
       should.deepEqual(pl.stats(), {
         tracks: 12,
         chars: {
-          en: 84813,
+          en: 84814,
           pli: 78866,
         },
         segments: {
